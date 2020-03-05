a5348820c2276d29e63321db294c81ad3579babf
Thomas Wagner <thomas.wagner3@etu.unistra.fr>
2020-03-05 12:22:02 +0100

    Modif de l'algo de positionnement des drapeaux

--- src/components/map/FlagsButtons.js
+++ src/components/map/FlagsButtons.js
@@ -9,11 +9,19 @@ function FlagsButtons({ setFlagsPositions, polygonPosition, forbiddenZones }) {
                     Supprimer les drapeaux
                 </button>
                 <button
-                    onClick={e =>
-                        setFlagsPositions(
-                            addRandomFlags(polygonPosition, forbiddenZones)
-                        )
-                    }
+                    onClick={e => {
+                        const flags = addRandomFlags(
+                            polygonPosition,
+                            forbiddenZones
+                        );
+                        flags.length != 10 &&
+                            alert(
+                                'Nous ne pouvons pas vous afficher plus de ' +
+                                    flags.length +
+                                    ' drapeaux dans cette zone. \nCelle-ci est probablement trop petite pour en contenir autant que vous souhaitiez.'
+                            );
+                        setFlagsPositions(flags);
+                    }}
                 >
                     Ajouter des drapeaux aléatoirement
                 </button>
