b09534db1c1ee88366998b488a4e6d84315b2edc
Clément KOENIG <clement.koenig@unistra.fr>
2020-03-20 16:24:46 +0100

    Add accordion

--- src/components/map/ForbiddenZoneActions.js
+++ src/components/map/ForbiddenZoneActions.js
@@ -1,42 +1,58 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Row, Col } from 'react-bootstrap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faPlus, faTrashAlt } from '@fortawesome/free-solid-svg-icons';
+import {
+    faPlus,
+    faTrashAlt,
+    faChevronUp,
+    faChevronDown
+} from '@fortawesome/free-solid-svg-icons';
 import { useForbiddenZone } from '../../utils/useForbiddenZone';
 
 function ForbiddenZoneActions({ action, setAction }) {
     const { forbiddenZones, createZone } = useForbiddenZone();
+    const [isOpen, setIsOpen] = useState(false);
 
     return (
         <>
-            <h3 className="mt-4">Zone interdites</h3>
-
-            <Row className="mt-3 ml-1">
-                <Col
-                    xs="auto"
-                    className={`mr-3 actions-item ${action ===
-                        'forbiddenZone' &&
-                        forbiddenZones.length === 0 &&
-                        'actions-item-selected'}`}
-                    onClick={() => {
-                        setAction('forbiddenZone');
-                        createZone();
-                    }}
-                >
-                    <FontAwesomeIcon icon={faPlus} />
-                </Col>
+            <Row
+                className="mt-4 ml-1 mr-1 justify-content-between align-items-center pointer"
+                onClick={() => setIsOpen(!isOpen)}
+            >
+                <h3>Zone interdites</h3>
+                <FontAwesomeIcon icon={isOpen ? faChevronDown : faChevronUp} />
             </Row>
 
-            <Row className="mt-2 ml-1">
-                {forbiddenZones.map((f, index) => (
-                    <ForbiddenZoneItem
-                        action={action}
-                        setAction={setAction}
-                        zone={f}
-                        key={index}
-                    />
-                ))}
-            </Row>
+            {isOpen && (
+                <>
+                    <Row className="mt-3 ml-1">
+                        <Col
+                            xs="auto"
+                            className={`mr-3 actions-item ${action ===
+                                'forbiddenZone' &&
+                                forbiddenZones.length === 0 &&
+                                'actions-item-selected'}`}
+                            onClick={() => {
+                                setAction('forbiddenZone');
+                                createZone();
+                            }}
+                        >
+                            <FontAwesomeIcon icon={faPlus} />
+                        </Col>
+                    </Row>
+
+                    <Row className="mt-2 ml-1">
+                        {forbiddenZones.map((f, index) => (
+                            <ForbiddenZoneItem
+                                action={action}
+                                setAction={setAction}
+                                zone={f}
+                                key={index}
+                            />
+                        ))}
+                    </Row>
+                </>
+            )}
         </>
     );
 }
--- src/components/map/MainZoneActions.js
+++ src/components/map/MainZoneActions.js
@@ -1,28 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Row, Col } from 'react-bootstrap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { useMainZone } from '../../utils/useMainZone';
-import { faPlus, faTrashAlt } from '@fortawesome/free-solid-svg-icons';
+import {
+    faPlus,
+    faTrashAlt,
+    faChevronDown,
+    faChevronUp
+} from '@fortawesome/free-solid-svg-icons';
 
 function MainZoneActions({ action, setAction }) {
-    const { position, removeAll } = useMainZone();
+    const { removeAll } = useMainZone();
+    const [isOpen, setIsOpen] = useState(true);
 
     return (
         <>
-            <h3>Zone de jeu</h3>
-            <Row className="mt-3 ml-1">
-                <Col
-                    xs="auto"
-                    className={`mr-3 actions-item ${action === 'mainZone' &&
-                        'actions-item-selected'}`}
-                    onClick={() => setAction('mainZone')}
-                >
-                    <FontAwesomeIcon icon={faPlus} />
-                </Col>
-                <Col xs="auto" className="actions-item" onClick={removeAll}>
-                    <FontAwesomeIcon icon={faTrashAlt} className="danger" />
-                </Col>
+            <Row
+                className="ml-1 mr-1 justify-content-between align-items-center pointer"
+                onClick={() => setIsOpen(!isOpen)}
+            >
+                <h3>Zone de jeu</h3>
+                <FontAwesomeIcon icon={isOpen ? faChevronDown : faChevronUp} />
             </Row>
+            {isOpen && (
+                <Row className="mt-3 ml-1">
+                    <Col
+                        xs="auto"
+                        className={`mr-3 actions-item ${action === 'mainZone' &&
+                            'actions-item-selected'}`}
+                        onClick={() => setAction('mainZone')}
+                    >
+                        <FontAwesomeIcon icon={faPlus} />
+                    </Col>
+                    <Col xs="auto" className="actions-item" onClick={removeAll}>
+                        <FontAwesomeIcon icon={faTrashAlt} className="danger" />
+                    </Col>
+                </Row>
+            )}
         </>
     );
 }
