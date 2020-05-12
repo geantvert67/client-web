3b6afa25ccb3a34b29aec550dcbde878da4664f9
Clément KOENIG <clement.koenig@unistra.fr>
2020-05-12 12:14:42 +0200

    Paginate games

--- src/components/games/GamesWrapper.js
+++ src/components/games/GamesWrapper.js
@@ -1,10 +1,31 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import InfiniteScroll from 'react-infinite-scroll-component';
 import { Container, Row, Col, Spinner } from 'react-bootstrap';
-import { useDataFromUrl } from '../../utils/data';
 import GamesListItem from './GamesListItem';
+import { getGames } from '../../service/user';
+
+const PAGE_SIZE = 15;
 
 function GamesWrapper() {
-    const { loading, error, data: games } = useDataFromUrl('/user/history');
+    const [hasMore, setHasMore] = useState(true);
+    const [games, setGames] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
+
+    useEffect(() => {
+        fetchGames()
+            .catch(err => setError(err))
+            .finally(() => setLoading(false));
+    }, []);
+
+    const fetchGames = () => {
+        return getGames(games.length / PAGE_SIZE, PAGE_SIZE).then(res => {
+            if (res.data.length < PAGE_SIZE) {
+                setHasMore(false);
+            }
+            setGames([...games, ...res.data]);
+        });
+    };
 
     return (
         <Container className="my-5">
@@ -21,9 +42,25 @@ function GamesWrapper() {
                     ) : error ? (
                         <p>Une erreur est survenue.</p>
                     ) : (
-                        games.map(game => (
-                            <GamesListItem key={game.id} game={game} />
-                        ))
+                        <InfiniteScroll
+                            dataLength={games.length}
+                            next={fetchGames}
+                            hasMore={hasMore}
+                            loader={
+                                <Row className="mt-3 justify-content-center">
+                                    <Col xs="auto">
+                                        <Spinner
+                                            animation="border"
+                                            variant="light"
+                                        />
+                                    </Col>
+                                </Row>
+                            }
+                        >
+                            {games.map(game => (
+                                <GamesListItem key={game.id} game={game} />
+                            ))}
+                        </InfiniteScroll>
                     )}
                 </Col>
             </Row>
--- src/service/user.js
+++ src/service/user.js
@@ -3,3 +3,7 @@ import request from '../utils/request';
 export const getByIdWithStats = id => {
     return request.get(`users/${id}/statistics`);
 };
+
+export const getGames = (page, pageSize) => {
+    return request.get(`/user/history?page=${page}&pageSize=${pageSize}`);
+};
